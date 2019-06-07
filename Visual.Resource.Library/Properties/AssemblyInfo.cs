faf0e6dc82c90b3d0f611de6f00e3b46625f72e4
Kurs Christian <40143278+Tipplin@users.noreply.github.com>
2019-06-07 18:47:25 +0200

    Add files via upload

---
+++ Visual.Resource.Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Pure Resources for all Projects.")]
+[assembly: AssemblyDescription("Visual.Resource.Library.dll")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Christian 'Tipplin' Kurs")]
+[assembly: AssemblyProduct("Visual.Resource.Library")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("Visual.Resource.Library.dll")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("842c6fa1-7093-4269-8dc4-5750c0b03f04")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.10000.1")]
+[assembly: AssemblyFileVersion("1.0.10000.1")]
